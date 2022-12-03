eee7d5eb9fb66a0b2612c7c42d90e4ba7e9e5a4b
David Tejuosho <tejuoshodavid@gmail.com>
2022-12-03 07:11:10 -0500

    format

--- Python/day2/puzzle1.py
+++ Python/day2/puzzle1.py
@@ -11,6 +11,7 @@ key = {"A": 1, "B": 2, "C": 3,
 score = 0
 for game in text.split("\n"):
     elf, me = game.split(" ")
+
     if key[elf] == key[me]:
         score += 3 + key[me]
     elif ((key[elf] - 2) % 3) + 1 == key[me]:
