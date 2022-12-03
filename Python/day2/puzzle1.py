cc4b2c5e57c919c7a62a6b92b075105c380e1570
David Tejuosho <tejuoshodavid@gmail.com>
2022-12-03 05:10:37 -0500

    python day 2

---
+++ Python/day2/puzzle1.py
@@ -0,0 +1,21 @@
+with open("input.txt", "r+") as file:
+    text = file.read()
+
+# text = """A Y
+# B X
+# C Z"""
+
+key = {"A": 1, "B": 2, "C": 3,
+       "X": 1, "Y": 2, "Z": 3}
+
+score = 0
+for game in text.split("\n"):
+    elf, me = game.split(" ")
+    if key[elf] == key[me]:
+        score += 3 + key[me]
+    elif ((key[elf] - 2) % 3) + 1 == key[me]:
+        score += key[me]
+    else:
+        score += 6 + key[me]
+
+print(score)
---
+++ Python/day2/puzzle2.py
@@ -0,0 +1,23 @@
+with open("input.txt", "r+") as file:
+    text = file.read()
+
+# text = """A Y
+# B X
+# C Z"""
+
+
+key = {"A": 1, "B": 2, "C": 3,
+       "X": 1, "Y": 2, "Z": 3}
+
+score = 0
+for game in text.split("\n"):
+    prev = score
+    elf, result = game.split(" ")
+    if result == "X":
+        score += 0 + ((key[elf] - 2) % 3) + 1
+    elif result == "Y":
+        score += 3 + key[elf]
+    else:
+        score += 6 + ((key[elf]) % 3) + 1
+
+print(score)
